4263a9b37ed09f2a3355f51ce94b8a831fd3ef98
RedAntDroid <dharunkumar2005cmd@gmail.com>
2025-03-06 09:59:37 +0530

    created the compenent

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,17 @@
-// 
+import React from "react";
+import "../App.css";
+const UserCard= () => {
+  return (
+    <div>
+
+      <h1 className="name">Dharun</h1>
+      <img className = "img" src="/image/myimage.jpeg" alt="" />
+      <p>mail: dharunkumar2005cmd@gmail.com</p>
+      <p>8610116218</p>
+      <p>kalapatti pirivu , coimbatore</p>
+      
+    </div>
+  );
+};
+
+export default UserCard;
